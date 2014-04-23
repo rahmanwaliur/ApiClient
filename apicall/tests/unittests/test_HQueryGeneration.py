6817bbd23be0e4baa0e626171eb8bbd6ceca69af
rahmanwaliur <waliur.rahman@ucalgary.ca>
2014-04-23 15:56:54 -0600

    test case for parseResponse of WorkFlowEngine

---
+++ apicall/tests/unittests/test_HQueryGeneration.py
@@ -0,0 +1,8 @@
+from unittest import TestCase
+
+__author__ = 'rahman'
+
+
+class TestHQueryGeneration(TestCase):
+    def test_generateQuery(self):
+        self.fail()
--- apicall/tests/unittests/test_workFlowEngine.py
+++ apicall/tests/unittests/test_workFlowEngine.py
@@ -1,11 +1,20 @@
-from unittest import TestCase
-
 __author__ = 'rahman'
 
 
+from unittest import TestCase
+
+from apicall.src.workflowengine import WorkFlowEngine
+
+
+
 class TestWorkFlowEngine(TestCase):
+
     def test_parseResponse(self):
-        self.fail()
+        response = [{u'cohortname': u'test1cohort', u'medcodes': [u'c68710004', u'c68710002', u'c68710001', u'c68710006', u'c68710005', u'c68710007'], u'userid': u'waliur', u'jobid': 10},{u'cohortname': u'test2cohort', u'medcodes': [u'c68710004', u'c68710002', u'c68710001', u'c68710006', u'c68710005', u'c68710007'], u'userid': u'jordan', u'jobid': 11}]
+        expectedresponse = {'cohort':[u'test1cohort', u'test2cohort'],'codes':[[u'c68710004', u'c68710002', u'c68710001', u'c68710006', u'c68710005', u'c68710007'], [u'c68710004', u'c68710002', u'c68710001', u'c68710006', u'c68710005', u'c68710007']] ,'user':[u'waliur', u'jordan'], }
+        responseObj = WorkFlowEngine()
+        receivedresponse = responseObj.parseResponse(response)
+        self.assertEqual(receivedresponse,expectedresponse)
 
     def test_generateQuery(self):
         self.fail()
